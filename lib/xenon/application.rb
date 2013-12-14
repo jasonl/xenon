5f74503a88fc0aa82659fce5c1b40173fc0aa63f
Jason Langenauer <jason@jasonlangenauer.com>
2013-12-14 15:52:41 +1100

    Wrap response body in an array

--- lib/xenon/application.rb
+++ lib/xenon/application.rb
@@ -1,5 +1,5 @@
 class Application
   def call(env)
-    return [200, {"Content-Type" => "text/html"}, "This is a test"]
+    return [200, {"Content-Type" => "text/html"}, ["This is a test"]]
   end
 end
