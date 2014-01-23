fd1c3f015fbb417f1a12bbc43ba0cc9e446fb24e
Jason Langenauer <jason@jasonlangenauer.com>
2014-01-23 17:13:54 +1100

    Add better error message for route not found

--- lib/xenon/application.rb
+++ lib/xenon/application.rb
@@ -23,6 +23,11 @@ module Xenon
       request = Rack::Request.new(env)
       method = request.request_method.to_sym
       action = routes.resolve_path(request.path, method, request.params)
+
+      if action.nil?
+        return [404, {'Content-Type' => 'text/html'}, ["Route #{method} #{request.path} not found"]]
+      end
+
       controller_name, action_name = action.split("#")
       controller_klass = Object.const_get(controller_name)
       controller = controller_klass.new(request)
@@ -31,7 +36,7 @@ module Xenon
         controller.send(action_name.to_sym)
         return controller.response
       else
-      return [404, {'Content-Type' => 'text/html'}, ["Action #{action_name} not found"]]
+        return [404, {'Content-Type' => 'text/html'}, ["Action #{action_name} not found"]]
       end
     end
 
